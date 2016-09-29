675a16a84f00522eb3a58310071c1ef071b37e6d
TheCjw <thecjw@live.com>
2016-09-29 23:04:57 +0800

    Add some options.

--- PsRevokeCerts/PsRevokeCerts.ps1
+++ PsRevokeCerts/PsRevokeCerts.ps1
@@ -7,17 +7,62 @@ function Test-IsAdmin {
   ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")
 }
 
-if (!(Test-IsAdmin)) {
-  Write-Host "Please run this script with admin priviliges" -ForegroundColor Red
+function Revoke-Cert ([string] $certFile, [int] $askUser) {
+  if (!(Test-Path (Join-Path Cert:\LocalMachine\Disallowed $info.Thumbprint))) {
+    if ($askUser -eq 1) {
+      $choise = Read-Host "[Y/N]"
+      if ($choise -eq "Y") {
+        $temp = Import-Certificate -FilePath $certFile -CertStoreLocation Cert:\LocalMachine\Disallowed
+      }
+    }
+    else {
+      $temp = Import-Certificate -FilePath $certFile -CertStoreLocation Cert:\LocalMachine\Disallowed
+    }
+  }
+  else {
+    Write-Host "This certificate is already revoked." -ForegroundColor Red
+  }
+}
+
+Write-Host "   ___       __                 _          ___          _       " -ForegroundColor DarkGray
+Write-Host "  / _ \___  /__\ _____   _____ | | _____  / __\___ _ __| |_ ___ " -ForegroundColor DarkGray
+Write-Host " / /_)/ __|/ \/// _ \ \ / / _ \| |/ / _ \/ /  / _ | '__| __/ __|" -ForegroundColor DarkGray
+Write-Host "/ ___/\__ / _  |  __/\ V | (_) |   |  __/ /__|  __| |  | |_\__ \" -ForegroundColor DarkGray
+Write-Host "\/    |___\/ \_/\___| \_/ \___/|_|\_\___\____/\___|_|   \__|___/" -ForegroundColor DarkGray
+Write-Host "An Powershell script to revoke CodeSigning certificates."
+Write-Host "  TheCjw, https://github.com/TheCjw/PsRevokeCerts"
+
+$certPath = Resolve-Path ".\Certificates\CodeSigning"
+
+if (!(Test-Path $certPath)) {
+  Write-Host "Please put this script in *\RevokeChinaCerts\Windows directory." -ForegroundColor Red
+  Exit
+}
+
+# Show menu.
+$message = "Make your choice:"
+
+$optionRevoke = New-Object System.Management.Automation.Host.ChoiceDescription "Revoke &All", `
+    "Revoke all CodeSigning certificates."
+
+$optionChoise = New-Object System.Management.Automation.Host.ChoiceDescription "&Choice", `
+    "Revoke CodeSigning certificates(Choice version)."
+
+$optionRestore = New-Object System.Management.Automation.Host.ChoiceDescription "&Restore All", `
+    "Restore all CodeSigning revoking."
+
+$optionsExit = New-Object System.Management.Automation.Host.ChoiceDescription "&Exit", `
+    "Just exit."
+
+$options = [System.Management.Automation.Host.ChoiceDescription[]]($optionRevoke, $optionChoise, $optionRestore, $optionsExit)
+
+$userChoice = $host.ui.PromptForChoice("", $message, $options, 0) 
+
+if ($userChoice -eq 3) {
   Exit
 }
 
 $regex = [regex]'(?<=CN=).*?(?=,\s(OU|O|L|S|C)=)'
-
-$currentPath = Get-Location
-
-$certPath = Join-Path $currentPath "\Certificates\CodeSigning"
-$certFiles = Get-ChildItem $certPath -Filter *.crt
 
 Get-ChildItem $certPath -Filter *.crt | ForEach-Object {
   $certFile = Join-Path $certPath $_
@@ -29,14 +74,24 @@ Get-ChildItem $certPath -Filter *.crt | ForEach-Object {
     $commonName = $result.Captures[0].value.Replace("`"", "")
   }
 
-  if (-Not (Test-Path (Join-Path Cert:\LocalMachine\Disallowed $info.Thumbprint))) {
-    Write-Host "Revoke" $commonName -ForegroundColor DarkYellow
-    Write-Host "  Valid from" $info.NotBefore "to" $info.NotAfter
-    Write-Host "  SerialNumber:" $info.SerialNumber
-    Write-Host "  Thumbprint:" $info.Thumbprint
-    $choise = Read-Host "[Y/N]"
-    if ($choise -eq "Y") {
-      $result = Import-Certificate -FilePath $certFile -CertStoreLocation Cert:\LocalMachine\Disallowed
+  Write-Host ("Processing {0}" -f $_) -ForegroundColor DarkGreen
+  Write-Host "  Common Name: " $commonName -ForegroundColor DarkYellow
+  Write-Host ("  Valid from {0} to {1}" -f $info.NotBefore, $info.NotAfter)
+  Write-Host "  SerialNumber:" $info.SerialNumber
+  Write-Host "  Thumbprint:" $info.Thumbprint
+
+  switch ($userChoice) {
+    0 { 
+      Revoke-Cert $certFile 0
+    }
+    1 {
+      Revoke-Cert $certFile 1
+    }
+    2 {
+      if (Test-Path (Join-Path Cert:\LocalMachine\Disallowed $info.Thumbprint)) {
+        Remove-Item (Join-Path Cert:\LocalMachine\Disallowed $info.Thumbprint)
+      }
     }
   }
+  Write-Host
 }
